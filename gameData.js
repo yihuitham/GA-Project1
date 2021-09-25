0014ad1fcf23f57909cd954239c440b5ad89e207
YihuiTham <yihuitham@gmail.com>
2021-09-25 13:51:08 +0800

    game over modal done

--- gameData.js
+++ gameData.js
@@ -1,6 +1,10 @@
 let $start = $("#start-btn");
+let $next = $(".next");
+let $previous = $(".previous");
+let $close = $(".close-button");
+let $restart = $("#reset-btn");
 let countdown;
-let timeLeft = 300 / reduceTimeScale;
+let timeLeft = 300 / reduceTimeScale / 10;
 
 function updateTime() {
   $(".time-left").text(timeLeft);
@@ -22,7 +26,7 @@ function updateBottles() {
   $(".display-no-bottles").text(character.bottles);
 }
 
-const timer = () => {
+function timer() {
   if (timeLeft > 0) {
     timeLeft--;
     $(".time-left").text(timeLeft);
@@ -30,34 +34,18 @@ const timer = () => {
     clearInterval(timeCounter);
     timeLeft = 60;
     console.log("times up");
+    startGame = false;
+    $("#overlay").css("opacity", "1");
+    openModal($(".game-over-modal"));
   }
-};
+}
 
-const startButton = () => {
+function startButton() {
   startGame = true;
   closeModal($(".instructions-modal"));
   $("#overlay").css("opacity", "0");
   timeCounter = setInterval(timer, 1000);
-};
-
-$start.on("click", startButton);
-
-$(".close-button").each(function () {
-  $(this).on("click", function () {
-    const modal = $(this).closest(".modal");
-    closeModal(modal);
-  });
-});
-
-$(".next").on("click", function () {
-  const page1 = $(".page1");
-  closeModal(page1);
-});
-
-$(".previous").on("click", function () {
-  const page1 = $(".page1");
-  openModal(page1);
-});
+}
 
 function closeModal(modal) {
   if (modal == null) return;
@@ -68,6 +56,31 @@ function openModal(modal) {
   if (modal == null) return;
   modal.css("transform", "translate(-50%, -50%) scale(1)");
 }
+
+$start.on("click", startButton);
+
+$close.each(function () {
+  $(this).on("click", function () {
+    const modal = $(this).closest(".modal");
+    closeModal(modal);
+  });
+});
+
+$next.on("click", function () {
+  const page1 = $(".page1");
+  closeModal(page1);
+});
+
+$previous.on("click", function () {
+  const page1 = $(".page1");
+  openModal(page1);
+});
+
+$restart.on("click", function () {
+  const gameOver = $(".game-over-modal modal");
+  closeModal(gameOver);
+  location.reload();
+});
 
 updateTime();
 updateCash();
